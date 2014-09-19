5f88961ded0e070d7d3b2379003112a4a983943d
Oscar Blanco <oscarbc@me.com>
2014-09-19 16:49:06 +0200

    Initial Commit

---
+++ Tarracopass/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tarracopass
+//
+//  Created by Oscar Blanco Castan on 19/09/14.
+//  Copyright (c) 2014 Oscar Blanco Castan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
