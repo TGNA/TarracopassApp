d1b2c9ada9dc602bace7d27b28e9b16c0c8d5456
Oscar Blanco <oscarbc@me.com>
2014-12-20 19:56:51 +0100

    added new menu

---
+++ Tarracopass/FCNavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  FCNavigationViewController.h
+//  Tarracopass
+//
+//  Created by Oscar Blanco Castan on 20/12/14.
+//  Copyright (c) 2014 Oscar Blanco Castan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCNavigationViewController : UIViewController
+
+@end
